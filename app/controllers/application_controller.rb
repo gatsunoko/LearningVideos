33798085c2dbd3e323f1e96d78f5082f9df0eb93
gatsunoko <vagrant@localhost.localdomain>
2016-11-01 18:48:54 +0900

    devise install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!	#deviseの認証
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'videos/index'
+  resources :videos
+  devise_for :users
 
-  get 'videos/show'
-
+  root 'videos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
