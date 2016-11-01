3049cdf47714e4756bba1942ba3a2b21a8ff5e80
gatsunoko <vagrant@localhost.localdomain>
2016-11-01 20:19:03 +0900

    Basic ver

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,50 @@
 class VideosController < ApplicationController
+
+	before_action :set_video, only: [:show, :edit, :update, :destroy]
+
   def index
+  	@videos = Video.all
+  end
+
+  def new
+  	@video = Video.new
+  end
+
+  def create
+  	@video = Video.new(video_params)
+
+  	if @video.save
+  		redirect_to @video
+  	else
+  		render 'new'
+  	end
   end
 
   def show
   end
+
+  def edit
+  end
+
+  def update
+  	if @video.update(video_params)
+  		redirect_to @video
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@video.destroy
+  	redirect_to videos_path
+  end
+
+  private
+  	def video_params
+  		params.require(:video).permit(:title, :videourl, :text)
+  	end
+
+  	def set_video
+  		@video = Video.find params[:id]
+  	end
 end
